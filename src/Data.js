e854cc220874ad627ce09141aac91ccf119956fa
abtyagi15 <abtyagi150702@gmail.com>
2023-06-19 08:22:00 +0530

    contains data which is used in cards

---
+++ src/Data.js
@@ -0,0 +1,90 @@
+const data = {
+  cards: [
+    {
+      cardType: "To Do",
+      numberOfCards: 4,
+      subCards: [
+        {
+          title: "Brainstorming",
+          description:
+            "Brainstorming brings team members' diverse experience into play.",
+          status: "low",
+          comments: "12",
+          files: "0",
+          invites: [
+            {
+              id: 1,
+              image: "invites.png",
+            },
+            {
+              id: 2,
+              image: "invites.png",
+            },
+            {
+              id: 3,
+              image: "invites.png",
+            },
+            {
+              id: 4,
+              image: "invites.png",
+            },
+          ],
+        },
+      ],
+    },
+    {
+        cardType: "On Progress",
+        numberOfCards: 3,
+        subCards: [
+          {
+            title: "Brainstorming",
+            description:
+              "Brainstorming brings team members' diverse experience into play.",
+            status: "low",
+            comments: "12",
+            files: "0",
+            invites: [
+              {
+                id: 1,
+                image: "invites.png",
+              },
+              {
+                id: 2,
+                image: "invites.png",
+              },
+              {
+                id: 3,
+                image: "invites.png",
+              },
+            ],
+          },
+        ],   
+    },
+    {
+        cardType: "Done",
+        numberOfCards: 2,
+      subCards: [
+        {
+          title: "Brainstorming",
+          description:
+            "Brainstorming brings team members' diverse experience into play.",
+          status: "low",
+          comments: "12",
+          files: "0",
+          invites: [
+            {
+              id: 3,
+              image: "invites.png",
+            },
+            {
+              id: 4,
+              image: "invites.png",
+            },
+          ],
+        },
+      ],
+    }
+  ],
+};
+
+export default data;
