26a49bea3427cff4ba7065ef865579afed3d1744
abtyagi15 <abtyagi150702@gmail.com>
2023-06-19 23:05:36 +0530

    Css is added

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,35 +9,54 @@ import UserImage from "../assests/user-image.png";
 import DropDownArrow from "../assests/arrow-down.png";
 const Header = () => {
   return (
-    <>
-      <div className="flex w-full items-center">
-        <div className="flex">
-          <img src={Logo} alt="logo" />
-          <h1>Project M.</h1>
-          <img src={dualArrow} alt="double arrow" />
+    <div className="header-container">
+      <div className="flex w-full items-center justify-between pl-[22px] pr-[48px]">
+        <div className="flex w-[400px]">
+          <img src={Logo} alt="logo" className="w-[24px] h-[24px]" />
+          <p className="text-[20px] font-semibold leading-[24px]">Project M.</p>
+          <img
+            src={dualArrow}
+            alt="double arrow"
+            className="w-[26px] h-[20px]"
+          />
         </div>
-        <div className="flex">
+        <div className="flex items-center justify-between w-full">
           <div className="flex search-container">
             <img src={Search} alt="search" />
             <p>Search for anything...</p>
           </div>
-          <div className="flex icon-container">
-            <img src={Calendar} alt="calendar" />
-            <img src={MessageQuestion} alt="Q&A" />
-            <img src={Notification} alt="notification" />
-          </div>
-          <div className="userData-container flex">
-            <div>
-              <p>Anima Agarwal</p>
-              <p>U.P,India</p>
+          <div className="flex">
+            <div className="flex icon-container">
+              <img src={Calendar} alt="calendar" />
+              <img src={MessageQuestion} alt="Q&A" />
+              <img src={Notification} alt="notification" />
             </div>
-            <img src={UserImage} alt="user" />
-            <img src={DropDownArrow} alt="drop down" />
+            <div className="userData-container flex items-center">
+              <div>
+                <p className="text-[16px] leading-[19px] font-normal text-[#0D062D]">
+                  Anima Agarwal
+                </p>
+                <p className="text-[14px] fon-normal leading-[17px] text-[#787486]">
+                  U.P,India
+                </p>
+              </div>
+              <img
+                src={UserImage}
+                alt="user"
+                className="w-[38px] h-[38px] rounded-full"
+                id="owner-user"
+              />
+              <img
+                src={DropDownArrow}
+                alt="drop down"
+                className="w-[18px] h-[18px]"
+              />
+            </div>
           </div>
         </div>
       </div>
       <div className="border border-[DBDBDB] "></div>
-    </>
+    </div>
   );
 };
 
